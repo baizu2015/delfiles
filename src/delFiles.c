955c68f61743d450d97d69c6e37e9f2a9fd69da8
yj@gmail.com <yj@gmail.com>
2016-07-06 21:53:38 +0800

    change method

--- src/delFiles.c
+++ src/delFiles.c
@@ -2,16 +2,23 @@
 #include<sys/stat.h>
 #include<dirent.h>
 #include<unistd.h>
+
+int delfile(char *);
+
 int main (void){
 	char *destDir="/usr/local/development/test";
+	delfile(destDir);
+	return 0;
+}
+int delfile(char *path){
 	DIR *dp;
 	struct dirent *entry;
 	struct stat statbuf;
-	if((dp = opendir(destDir)) == NULL){
-		fprintf(stderr,"cannot open director:%s \n",destDir);
+	if((dp = opendir(path)) == NULL){
+		fprintf(stderr,"cannot open director:%s \n",path);
 		return -1;
 	}
-	chdir(destDir);
+	chdir(path);
 	while((entry=readdir(dp))!=NULL){
 		lstat(entry->d_name,&statbuf);
 		if(S_ISREG(statbuf.st_mode)){
